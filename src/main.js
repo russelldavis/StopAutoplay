7e552a3548d4dfe72f4cd832e14290304bda3994
Russell Davis <551404+russelldavis@users.noreply.github.com>
2021-08-31 16:19:54 -0700

    Simplify so it only stops autoplay in the background
    Fixes issues with the focusStop behavior that sometimes stopped videos
    in the foreground

--- src/main.js
+++ src/main.js
@@ -1,25 +1,10 @@
 ;(function StopAutoplay(window) {
 	'use strict'
 
-	/** @version 4.1.0 **/
-	const document = window.document,
-		extended = false
+	const document = window.document;
 
-	/**
-	 * Non-Extended: When a tab has been opened as background tab for the first time, the video is loaded when the tab receives focus (Chrome native feature)
-	 * @type {Boolean}
-	 */
-	let focusStop = false,
-		seeked = false
-
-	/**
-	 * Issues the pause command on the player element.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
 	function _pause(player) {
 		console.log('pause', player, player.currentTime)
-
 		if (player.pause !== undefined) {
 			player.pause()
 			return
@@ -27,140 +12,27 @@
 		player.pauseVideo() // Flash API
 	}
 
-	/**
-	 * Stops the player, when user didn't seek, the tab does not have focus, looping is disabled and it isn't a playlist page.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
 	function stopAutoplay(player) {
 		console.log(
 			'stopAutoplay',
 			!player.loop,
 			document.location.search.indexOf('list=') === -1,
 			document.hidden,
-			focusStop,
 			player
 		)
-		if (seeked) {
-			console.log('stopAutoplay seeked')
-			seeked = false
-
-			return seeked // false
-		}
-
+		// don't stop looping videos or playlists
 		if (
-			(!player.loop &&
-			document.location.search.indexOf('list=') === -1 && // we don't want to stop looping videos or playlists
-				!document.hasFocus()) || // is video in background?
-			focusStop // bg tab override
+			!player.loop &&
+			document.location.search.indexOf('list=') === -1 &&
+			!document.hasFocus()
 		) {
 			_pause(player)
 			return true
 		}
-
 		return false
 	}
 
-	/**
-	 * Issues the play command on the player element.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
-	function _play(player) {
-		console.log('play', player)
-
-		if (player.play !== undefined) {
-			const maybePromise = player.play()
-			if (maybePromise instanceof Promise) maybePromise.catch(console.warn)
-			return
-		}
-		player.playVideo() // Flash API
-	}
-
-	/**
-	 * Plays a video when the tab is not hidden.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
-	function _playIfNotHidden(player) {
-		if (!document.hidden) _play(player)
-	}
-
-	/**
-	 * Event handler when the tab becomes visible.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
-	function handleVisibilityChange(player) {
-		console.log('handleVisibilityChange', player, player.readyState)
-		// if (player.readyState < 1) return; // bail out, if event triggered too early
-		window.setTimeout(_playIfNotHidden.bind(undefined, player), 60)
-	}
-
-	/**
-	 * Adds listeners for debugging purposes.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
-	function addDebugListener(player) {
-		player.addEventListener('canplay', function() {
-			console.log('canplay')
-		})
-		player.addEventListener('canplaythrough', function() {
-			console.log('canplaythrough')
-		})
-		player.addEventListener('durationchange', function() {
-			console.log('durationchange')
-		})
-		player.addEventListener('loadeddata', function() {
-			console.log('loadeddata')
-		})
-		player.addEventListener('loadedmetadata', function() {
-			console.log('loadedmetadata')
-		})
-		player.addEventListener('loadstart', function() {
-			console.log('loadstart')
-		})
-		player.addEventListener('playing', function() {
-			console.log('playing')
-		})
-		player.addEventListener('play', function() {
-			console.log(player.readyState, player.networkState)
-			console.log('play')
-		})
-		player.addEventListener('waiting', function() {
-			console.log('waiting')
-		})
-		player.addEventListener('stalled', function() {
-			// console.log('stalled')
-		})
-		player.addEventListener('seeking', function() {
-			console.log('seeking')
-		})
-		player.addEventListener('seeked', function() {
-			// user seeked forward / backward
-			console.log('seeked')
-		})
-		player.addEventListener('timeupdate', function() {
-			// console.log('timeupdate')
-		})
-		window.addEventListener('focus', function() {
-			// tells me the timestamp I've focused
-			console.info('focus now')
-		})
-		document.addEventListener('visibilitychange', function() {
-			// tells me the timestamp I've focused
-			console.info('visibilitychange now')
-		})
-	}
-
 	const boundPlayers = new WeakSet()
-
-	/**
-	 * Binds player specific events.
-	 *
-	 * @param {HTMLVideoElement} player
-	 */
 	function bindPlayer(player) {
 		if (boundPlayers.has(player)) return
 		boundPlayers.add(player)
@@ -173,62 +45,11 @@
 			stopAutoplay(player)
 		}
 
-		console.info('add debug', addDebugListener(player))
-
 		/** Main stop function */
 		player.addEventListener(
 			'canplaythrough',
 			stopAutoplay.bind(undefined, player)
 		)
-
-		/** Stops on watch -> watch navigation */
-		let i = 0
-		player.addEventListener('playing', function playing() {
-			console.log('stop on playing event')
-
-			stopAutoplay(player)
-			if (++i === 2) player.removeEventListener('playing', playing)
-		})
-
-		player.addEventListener('loadeddata', function() {
-			console.log('loadeddata -> reset counter')
-
-			i = 0 // reset; watch -> watch navigation
-		})
-
-		/** Handler for the "Extended" version. */
-		if (extended)
-			window.addEventListener(
-				'focus',
-				handleVisibilityChange.bind(undefined, player)
-			)
-		else {
-			/** Shouldn't stop when seeking */
-			const seekedTrue = () => {
-				if (player.readyState > 1) seeked = true
-			}
-			const onfocus = function() {
-				focusStop = true
-				player.addEventListener(
-					'playing',
-					() => {
-						focusStop = false
-					},
-					{ once: true }
-				)
-
-				/** Shouldn't stop when clicking play for the first time */
-				player.addEventListener('play', seekedTrue, { once: true })
-				player.addEventListener('seeked', seekedTrue)
-				/** Don't pause when slow internet speed */
-				player.addEventListener('waiting', seekedTrue)
-			}
-
-			/** When a tab has been opened as background tab for the first time, the video is loaded when the tab receives focus (Chrome native feature) */
-			document.hasFocus()
-				? onfocus()
-				: window.addEventListener('focus', onfocus, { once: true })
-		}
 	}
 
 	/**
@@ -237,8 +58,8 @@
 	function waitForPlayer() {
 		const observer = new MutationObserver(function(mutations) {
 			for (let i = 0; i < mutations.length; ++i) {
-				var mutation = mutations[i].addedNodes
-				for (var x = 0; x < mutation.length; ++x) {
+				let mutation = mutations[i].addedNodes
+				for (let x = 0; x < mutation.length; ++x) {
 					if (
 						mutation[x].nodeName !== 'VIDEO' &&
 						mutation[x].nodeName !== 'OBJECT'
